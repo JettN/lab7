45d4c5073d8004f96a94d45013a005ba94eb5a7c
Jett N Ngo <cs15lwi23amn@ieng6-201.ucsd.edu>
2023-02-27 20:07:14 -0800

    Em

--- ListExamples.java
+++ ListExamples.java
@@ -18,6 +18,7 @@ class ListExamples {
     return result;
   }
 
+
   // Takes two sorted list of strings (so "a" appears before "b" and so on),
   // and return a new list that has all the strings in both list in sorted order.
   static List<String> merge(List<String> list1, List<String> list2) {
@@ -39,7 +40,7 @@ class ListExamples {
     }
     while(index2 < list2.size()) {
       result.add(list2.get(index2));
-      index2 += 1;
+      index1 += 1;
     }
     return result;
   }
